e74a467684e7256ac5ea4d8b755f7c02ff783c2b
AhmadNuzulurRozaq <nuzulnazil14@gmail.com>
2025-11-27 20:35:25 +0700

    Perbaikan Header dan Footer

--- resources/views/galeri/galeri.blade.php
+++ resources/views/galeri/galeri.blade.php
@@ -3,31 +3,104 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Galeri | UKM Labbaik</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous" defer></script>
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/header&footer.css')}}">
 </head>
-<body>
-    <header class="">
-        <nav class="navbar navbar-sm navbar-expand-sm bg-dark py-3">
-            <div class="container">
-                <div class="image">
-                    <img src="{{ asset('images/logo/logo.png') }}" alt="UKM LAbbaik Logo" style="width: 60px" class="img-fluid">
+    <body>
+        <!-- MENU HEADER -->
+            <header>
+                <div class="logo">
+                    <img src="{{ asset('/images/logo/logopolije.png') }}" alt="LOGO POLIJE">
+                    <img src="{{ asset('/images/logo/logoukm.png') }}" alt="LOGO UKM">
                 </div>
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="#">Link 1</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="#">Link 2</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="#">Link 3</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </header>
-</body>
+
+                <div class="menuNavigasi">
+                    <nav class="menu">
+                        <ul>
+                            <li><a href="#">PROFILE</a></li>
+                            <li><a href="#">GALERI</a></li>
+                            <li class="dropdown">
+                                <button class="dropButton">DEPARTEMEN ⇂
+                                    <i class="fa fa-caret-down"></i>
+                                </button>
+                                <div class="dropdown-content">
+                                    <a href="#">DEPARTEMEN PSDM</a>
+                                    <a href="#">DEPARTEMEN SYIAR</a>
+                                    <a href="#">DEPARTEMEN KEMUSLIMAHAN</a>
+                                    <a href="#">DEPARTEMEN KEWIRAUSAHAAN</a>
+                                    <a href="#">DEPARTEMEN KOMINFO</a>
+                                </div>
+                            </li>
+                            <li><a href="#">KONTAK</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+            <main>
+            <!-- ISI KONTEN WEBSITE ANDA -->
+            </main>
+            {{-- MENU FOOTER --}}
+            <footer>
+                <div class="isiFooter">
+                    <div class="logoUKM">
+                        <img src="{{ asset('/images/logo/logoukm.png') }}" alt="LOGO UKM">
+                    </div>
+                    <div class="infoFooter">
+                        <span>UNIT KEGIATAN MAHASISWA</span>
+                        <h1>LABBAIK POLIJE</h1>
+                        <span><i>"Salam Labbaik !!! | Bersama Labbaik Ayo Lebih Baik..."</i></span>
+                        <hr>
+                        <div class="icon">
+                            <div class="address">
+                                <img src="{{ asset('/images/logo/location-white.svg') }}" alt="Location Icon">
+                                <span>Jl. Mastrip POBOX 164 <br>
+                                    Sumbersari - Jember</span>
+                            </div>
+                            <div class="phone">
+                                <img src="{{ asset('/images/logo/phone-white.svg') }}" alt="Phone Icon">
+                                <span>0821-4313-1449</span>
+                            </div>
+                            <div class="email">
+                                <img src="{{ asset('/images/logo/email-white.svg') }}" alt="Email Icon">
+                                <span>ukm.labaik@polije.ac.id</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="menuFooter">
+                        <div class="menuNavbar">
+                            <ul>
+                                <li><h3>MENU</h3></li>
+                                <li><a href="#">PROFILE</a></li>
+                                <li><a href="#">GALERI</a></li>
+                                <li><a href="#">DEPARTEMEN</a></li>
+                                <li><a href="#">KONTAK</a></li>
+                            </ul>
+                        </div>
+                        <div class="menuDepartemen">
+                            <ul>
+                                <li><h3>DEPARTEMEN</h3></li>
+                                <li><a href="#">DEPARTEMEN PSDM</a></li>
+                                <li><a href="#">DEPARTEMEN SYIAR</a></li>
+                                <li><a href="#">DEPARTEMEN KEMUSLIMAHAN</a></li>
+                                <li><a href="#">DEPARTEMEN KEWIRAUSAHAAN<a></li>
+                                <li><a href="#">DEPARTEMEN KOMINFO</a></li>
+                            </ul>
+                        </div>
+                        <div class="mediaSosial">
+                            <h3>MEDIA SOSIAL</h3>
+                            <span>Jangan lupa join juga media sosial kami dibawah ini.</span>
+                            <div class="sosmedIicon">
+                                <a href="#"><img src="{{ asset('/images/logo/instagram-white.svg')}}" alt="instagram"></a>
+                                <a href="#"><img src="{{ asset('/images/logo/tiktok-white.png')}}" alt="tiktok"></a>
+                                <a href="#"><img src="{{ asset('/images/logo/youtube-white.svg')}}" alt="youtube"></a>
+                                <a href="#"><img src="{{ asset('/images/logo/facebook-white.svg') }}" alt="facebook"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="copyright">
+                    <span>Copyright &copy; 2025 Labbaik Team</span>
+                </div>
+            </footer>
+    </body>
 </html>
